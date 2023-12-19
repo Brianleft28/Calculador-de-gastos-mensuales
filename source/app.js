3c68b9b32c087a8e977538829ec7bb205ecf97c7
Brianleft28 <contactobrianleft@gmail.com>
2023-12-19 15:54:13 -0300

    Almost finish

--- source/app.js
+++ source/app.js
@@ -1,24 +1,37 @@
+let presupuestoTotal = 0;
+
 const presupuestoBtn = document.getElementById("btnPresupuesto");
+const imgJsDinero = document.getElementsByClassName("img")[0];
+const divPrespuesto = document.getElementById("collapseExample");
 
-const divPrespuesto = document.getElementById('collapseExample');
-let presupuestoTotal = 0
+// presupuesto en local storage //
+const presupuestoLocalStorage = localStorage.getItem("presupuesto");
 
-
-
-function modificarPresupuesto(presupuestoTotal){
-  if(presupuesto) {
-    const info = document.getElementsByTagName('small')[0];
-    info.textContent = `Presupuesto disponible: $ ${presupuestoTotal}`
-    info.marginTop = '14px' 
-    info.style.display = 'block'
-  } 
+if (presupuestoLocalStorage) {
+  presupuestoTotal = parseFloat(presupuestoLocalStorage);
+  modificarPresupuesto(presupuestoTotal);
+  const tituloBanner = document.getElementsByTagName("h6")[0];
+  tituloBanner.style.display = "none";
 }
 
+function modificarPresupuesto(presupuestoTotal) {
+  const info = document.getElementsByTagName("h1")[0];
+
+  if (presupuestoTotal) {
+    info.textContent = `Presupuesto actual: $${presupuestoTotal}`;
+    info.marginTop = "14px";
+    info.style.display = "block";
+
+    localStorage.setItem("presupuesto", presupuestoTotal.toString());
+  } else if (presupuestoTotal <= 0) {
+    info.textContent = `Te quedaste en 0 flaco.
+                        Presupuesto Actual: ${presupuestoTotal}.`;
+  }
+}
 
 // Evento del boton presupuesto
 presupuestoBtn.addEventListener("click", (x) => {
-   // Tomo el valor del input, cambio el contenido del primer div por la información del presupuesto
-  
+  // Tomo el valor del input, cambio el contenido del primer div por la información del presupuesto
 
   if (presupuesto) {
     const presupuestoInput = document.getElementById("presupuesto");
@@ -26,128 +39,132 @@ presupuestoBtn.addEventListener("click", (x) => {
 
     if (!isNaN(nuevoPresupuesto)) {
       presupuestoTotal = nuevoPresupuesto;
-      modificarPresupuesto(presupuestoTotal)
-      const imgJsDinero = document.getElementsByClassName("img")[0];
-    if(imgJsDinero){
-      imgJsDinero.src = "./asset/img/dinero.png";
-    } else {
-      console.error("El elemento con ese className no se encontró");
-
-    }
-      
-      const tituloPrimerBanner = document.getElementsByTagName('h6')[0];
-      const textoBannerJs = document.getElementsByTagName('small')[0];
-
-      tituloPrimerBanner.innerText = ''
-      textoBannerJs.style.display = 'none';
-      
+      // almacenamos en el localStorage //
       modificarPresupuesto(presupuestoTotal);
 
-    } 
-    else {
+      if (imgJsDinero) {
+        const src = "./asset/img/dinero.png";
+        imgJsDinero.src = src;
+      }
+
+      const tituloPrimerBanner = document.getElementsByTagName("h6")[0];
+
+      tituloPrimerBanner.innerText = "";
+
+      modificarPresupuesto(presupuestoTotal);
+    } else {
     }
-    
-    divPrespuesto.style.display = 'none';
 
+    divPrespuesto.style.display = "none";
   }
-// btn cancelar presupuesto
+  // btn cancelar presupuesto
 
-const btnCancelar = document.getElementById('btnCancelarPresu')
+  const btnCancelar = document.getElementById("btnCancelarPresu");
 
-  btnCancelar.addEventListener('click', (e)=>{
-    divPrespuesto.style.display = 'none';
-  })
+  btnCancelar.addEventListener("click", (e) => {
+    divPrespuesto.style.display = "none";
+  });
 
-  const btnPresu = document.querySelector('#btnPresu');
-  
-  btnPresu.addEventListener('click', (e)=>{
-    const divPrespuesto = document.getElementById('collapseExample');
-    
-    divPrespuesto.style.display = 'block';
-  })
+  const btnPresu = document.querySelector("#btnPresu");
 
+  btnPresu.addEventListener("click", (e) => {
+    const divPrespuesto = document.getElementById("collapseExample");
+
+    divPrespuesto.style.display = "block";
+  });
 });
 
 // mostrando los gastos
 // Suponiendo que btnGasto es el botón y nombreDeGasto es el campo de entrada
-const btnGasto = document.getElementById('btnGasto');
-const nombreDeGastoInput = document.getElementById('nombreDeGasto');
-const valorDeGastoInput = document.getElementById('costoDeGasto')
+const btnGasto = document.getElementById("btnGasto");
+const nombreDeGastoInput = document.getElementById("nombreDeGasto");
+const valorDeGastoInput = document.getElementById("costoDeGasto");
 
-
-
-btnGasto.addEventListener('click', (x) => {
+btnGasto.addEventListener("click", (e) => {
   // Obtener el valor del campo de entrada
-  const nombreDeGasto = nombreDeGastoInput.value;
-  const valorDeGasto = parseFloat(valorDeGastoInput.value);
 
-  
+  const valorDeGasto = parseFloat(valorDeGastoInput.value);
+  const nombreDeGasto = nombreDeGastoInput.value;
+
   if (isNaN(valorDeGasto) || valorDeGasto <= 0) {
-    alert('Por favor, ingrese un valor numérico válido para el gasto.');
+    alert("Por favor, ingrese un valor numérico válido para el gasto.");
     return;
   }
 
-     if(nombreDeGasto === '') {
-    alert('Por favor, ingrese un nombre para el gasto.');
-    return
-  } else if(valorDeGasto > presupuestoTotal) {
-    alert('El gasto no puede ser mayor que el presupuesto disponible')
-    return
+  if (nombreDeGasto === "") {
+    alert("Por favor, ingrese un nombre para el gasto.");
+    return;
+  } else if (valorDeGasto > presupuestoTotal) {
+    alert("El gasto no puede ser mayor que el presupuesto disponible");
+    return;
   }
 
-    // Crear un nuevo elemento de lista
-    const listaGastos = document.createElement('li');
-    listaGastos.classList = ('gastosLi')
+  // Crear un nuevo elemento de lista
+  const listaGastos = document.createElement("li");
+  listaGastos.classList = "gastosLi";
+
+  const ID = Date.now();
+  listaGastos.setAttribute("data-id", ID);
+
+  // Establecer el contenido de texto del elemento de lista
+
+  presupuestoTotal -= valorDeGasto;
+  modificarPresupuesto(presupuestoTotal);
+
+  listaGastos.textContent = `Gasto: ${nombreDeGasto} || Valor: $${valorDeGasto}`;
+
+  const divGastos = document.querySelector(".listaGastos");
+  divGastos.classList = "col-12 col-lg-12 col-md-12 listaGastos";
+  divGastos.style.display = "block";
+
+  // Agregar el nuevo elemento de lista al contenedor
+
+  divGastos.appendChild(listaGastos);
+
+  // debo agregarle un boton para editar, y uno para borrarlo de la lista.
+  let btnEditar = document.createElement("button");
+  btnEditar.textContent = "Editar";
+  btnEditar.classList = "btn btn-primary btnEditar";
+
+  listaGastos.appendChild(btnEditar);
+
+  let btnBorrar = document.createElement("button");
+  btnBorrar.textContent = "Eliminar";
+  btnBorrar.classList = "btn btn-danger btnBorrar";
+
+  listaGastos.appendChild(btnBorrar);
+  const divCollapse2 = document.getElementById("collapseExample2");
+
+  divCollapse2.style.display = "none";
+
+  btnBorrar.addEventListener("click", (e) => {
+    divGastos.removeChild(listaGastos);
+
+    presupuestoTotal = +valorDeGasto;
+
+    const arrayGastos = []
+    arrayGastos.push(nombreDeGasto, nombreDeGasto)
     
-    // Establecer el contenido de texto del elemento de lista
-    presupuestoTotal -= valorDeGasto;
-    modificarPresupuesto(presupuestoTotal)
-    
-    listaGastos.textContent = `${nombreDeGasto}: $${valorDeGasto} || Presupuesto Actual: $${presupuestoTotal}`;
-    // modificando el presupuesto general
-    // Obtener el contenedor de la lista (sustituye '.listaGastos' con tu clase real)
-    const divGastos = document.querySelector('.listaGastos');
-    divGastos.classList = 'col-12 col-lg-12 col-md-6 listaGastos'
-    divGastos.style.display = 'block'
+    console.table(arrayGastos)
 
-    // Agregar el nuevo elemento de lista al contenedor
-    divGastos.appendChild(listaGastos); 
-    // debo agregarle un boton para editar, y uno para borrarlo de la lista. 
-    let btnEditar = document.createElement('button') 
-    btnEditar.textContent = "Editar"  
-    btnEditar.classList = 'btn btn-primary btnEditar'
-    
-    listaGastos.appendChild(btnEditar);
+  });
+});
 
-    let btnBorrar = document.createElement('button')
-    btnBorrar.textContent = "X"
-    btnBorrar.classList = 'btn btn-danger btnEditar'
-    
-    listaGastos.appendChild(btnBorrar);
-    const divCollapse2 = document.getElementById('collapseExample2')
+const btnCancelarGasto = document.getElementById("btnCancelarGasto");
 
-    divCollapse2.style.display = 'none'
+btnCancelarGasto.addEventListener("click", (e) => {
 
-    // Actualizar presupuesto en el titulo 
-    const divPresu = document.getElementById('divPresu');
+  const divCollapse2 = document.getElementById("collapseExample2");
+  divCollapse2.style.display = "none";
 
-  })
+});
 
+  
+  
+const btnInputGasto = document.getElementById("gastoInputBtn");
 
-const btnCancelarGasto = document.getElementById('btnCancelarGasto');
+btnInputGasto.addEventListener("click", (e) => {
+  const divCollapse2 = document.getElementById("collapseExample2");
 
-btnCancelarGasto.addEventListener('click',(e)=>{
-  const divCollapse2 = document.getElementById('collapseExample2')
-
-  divCollapse2.style.display = 'none'
-})
-
-const btnInputGasto = document.getElementById('gastoInputBtn');
-
-btnInputGasto.addEventListener('click', (e)=> {
-  const divCollapse2 = document.getElementById('collapseExample2');
-
-  divCollapse2.style.display = 'block';
-})
-
-
+  divCollapse2.style.display = "block";
+});
